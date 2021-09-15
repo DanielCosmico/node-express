e2efb55a123949d43cbb8dc280c2a65bf6716120
Daniel Lopez <a315120@uach.mx>
2021-09-15 00:06:15 -0500

    results

--- routes/results.js
+++ routes/results.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/users')
+const controller = require('../controllers/results')
 
 /* GET users listing. */
 router.get('/:n1/:n2', controller.suma);
